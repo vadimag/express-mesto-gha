d6a54bbd6eca40f2230eb742e430a4bd1acfd3f8
vadimag <wada3@yandex.ru>
2022-09-13 12:05:49 +0500

    commit 5

--- controllers/users.js
+++ controllers/users.js
@@ -19,8 +19,12 @@ const getUserById = (req, res) => {
       }
       res.send(user);
     })
-    .catch(() => {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при поиске пользователя' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
     });
 };
 
