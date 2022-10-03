b6fd82ed18651f8a8ec0e54480b622bc832f21ad
vadimag <wada3@yandex.ru>
2022-10-03 18:39:18 +0500

    commit 3

--- moddlewares/auth.js
+++ moddlewares/auth.js
@@ -6,29 +6,6 @@ const KEY = 'fgsdsdjgskd';
 
 const getToken = (id) => jwt.sign({ _id: id }, KEY, { expiresIn: '7d' });
 
-// const auth = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   // if (!authorization || !authorization.startsWith('Bearer ')) {
-//   if (!authorization) {
-//     return res.status(401).send({ message: 'Необходима авторизация' });
-//   }
-//   const token = authorization.replace('Bearer ', '');
-//   return jwt.verify(token, KEY, (err, payload) => {
-//     if (err) {
-//       return res.status(401).send({ message: 'Необходима авторизация' });
-//     }
-//     return User.findOne({ _id: payload._id })
-//       .then((user) => {
-//         if (!user) {
-//           return res.status(401).send({ message: 'Необходима авторизация' });
-//         }
-//         req.user = user._id;
-//         next();
-//       })
-//       .catch(() => res.status(401).send({ message: 'Необходима авторизация' }));
-//   });
-// };
-
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
-const BadRequestError = require('../errors/bad-request-err');
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -50,12 +50,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new BadRequestError('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new BadRequestError('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
