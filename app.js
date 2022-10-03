439a07bf218dd48da3c202d6e8b0e4bc39b2b61e
vadimag <wada3@yandex.ru>
2022-10-03 15:48:25 +0500

    commit 4

--- app.js
+++ app.js
@@ -37,8 +37,13 @@ app.use('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-  }).unknown(true),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+  }),
 }), createUser);
+
+// app.use('/signup', createUser);
 
 app.use(auth);
 app.use('/users', routerUsers);
