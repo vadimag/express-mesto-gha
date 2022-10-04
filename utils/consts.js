c3b246a47a40b403eb6427ed218f6fbc79553e46
vadimag <wada3@yandex.ru>
2022-10-04 22:53:48 +0500

    commit 15

--- utils/consts.js
+++ utils/consts.js
@@ -1,4 +1,4 @@
-const urlRegex = /((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/g;
+const urlRegex = /((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
 
 // eslint-disable-next-line import/prefer-default-export
 module.exports = { urlRegex };
